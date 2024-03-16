517b5857ef56084046067e8c6478d36db381956f
Natalya <soltk7@yandex.ru>
2024-03-16 14:22:14 +0300

    result v.1

--- export_csv.py
+++ export_csv.py
@@ -2,7 +2,7 @@ def create(datalist):
     csv = 'id,text\n'
     for d in datalist:
         csv += '{}\n'.format(','.join(str(el) for el in d))
-
+        print(csv)
     with open('resultdata.csv','w', encoding="utf-8") as page:
         page.write(csv)
     return csv
